f514c912f20669eb01b70074e5430eaef793443b
Lucas Corte <corte1994.lc@gmail.com>
2021-05-22 20:10:53 -0300

    aula e exercicios

---
+++ 3 - native, host e user/scripts.js
@@ -0,0 +1,10 @@
+// Liste 5 objetos nativos
+Object, String, Array, Number, Math
+
+// Liste 5 objetos do browser
+Window, history, document, HTMLCollection, NodeList
+
+// Liste 2 Métodos, Propriedades ou Objetos
+// presentes no Chrome que não existem no Firefox
+
+/*Funções Webkit do Chrome*/
