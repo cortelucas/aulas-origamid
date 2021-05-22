df0f7c3cf30cc21193b4bb993e70338bc43859db
Lucas Corte <corte1994.lc@gmail.com>
2021-05-22 20:26:59 -0300

    aula 02 de strings

---
+++ 4 - Strings/scripts.js
@@ -0,0 +1,27 @@
+// const comida = 'Pizza';
+// const agua = new String('Água');
+
+// console.log(agua.length);
+
+// const frase = 'A melhor comida';
+// console.log(frase [4]);
+
+const frase = 'A melhor linguagem é ';
+const linguagem = 'JavaScript';
+
+console.log(frase.concat(linguagem, '!!'));
+
+const fruta = 'Banana';
+const listaFrutas = 'Melancia, Banana, Laranja';
+
+console.log(listaFrutas.includes(fruta));
+console.log(fruta.endsWith('na'));
+console.log(fruta.slice(0,3));
+console.log(fruta.slice(-3));
+console.log(fruta.indexOf('B'));
+
+const listaPrecos = ['R$ 99', 'R$ 199', 'R$ 12000'];
+
+listaPrecos.forEach((preco) => {
+  console.log(preco.padStart(10, '.'));
+})
