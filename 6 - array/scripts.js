46178ef75fcd3a0b0d77aef58c2ac9445c29a999
Lucas Corte <corte1994.lc@gmail.com>
2021-05-23 14:07:03 -0300

    aula arrays 2

--- 6 - array/scripts.js
+++ 6 - array/scripts.js
@@ -62,3 +62,56 @@ const ultimoCarro = carros.pop(); // 'Honda'
 carros; // ['Fiat', 'VW'];
 
 carros.reverse(); // ['Honda', 'VW', 'Fiat', 'Ford'];
+
+carros.splice(1, 0, 'Kia', 'Mustang'); // []
+carros; // ['Ford', 'Kia', 'Mustang', 'Fiat', 'VW', 'Honda']
+
+carros.splice(3, 2, 'Ferrari'); // ['Fiat', 'VW']
+carros; // ['Ford', 'Kia', 'Mustang', 'Ferrari', 'Honda']
+
+['Item1', 'Item2', 'Item3', 'Item4'].copyWithin(2, 0, 3);
+// ['Item1', 'Item2', 'Item1', 'Item2']
+
+['Item1', 'Item2', 'Item3', 'Item4'].copyWithin(-1);
+// ['Item1', 'Item2', 'Item3', 'Item1']
+
+['Item1', 'Item2', 'Item3', 'Item4'].fill('Banana');
+// ['Banana', 'Banana', 'Banana', 'Banana']
+
+['Item1', 'Item2', 'Item3', 'Item4'].fill('Banana', 2);
+// ['Item1', 'Item2', 'Banana', 'Banana']
+
+['Item1', 'Item2', 'Item3', 'Item4'].fill('Banana', 1, 3);
+// ['Item1', 'Banana', 'Banana', 'Item4']
+
+const transporte1 = ['Barco', 'Aviao'];
+const transporte2 = ['Carro', 'Moto'];
+const transportes = transporte1.concat(transporte2);
+// ['Barco', 'Aviao', 'Carro', 'Moto'];
+
+const maisTransportes = [].concat(transporte1, transporte2, 'Van');
+// ['Barco', 'Aviao', 'Carro', 'Moto', 'Van'];
+
+const linguagens = ['html', 'css', 'js', 'php', 'python', 'js'];
+
+linguagens.includes('css'); // true
+linguagens.includes('ruby'); // false
+linguagens.indexOf('python'); // 4
+linguagens.indexOf('js'); // 2
+linguagens.lastIndexOf('js'); // 5
+
+const linguagens = ['html', 'css', 'js', 'php', 'python'];
+linguagens.join(); // 'html,css,js,php,python'
+linguagens.join(' '); // 'html css js php python'
+linguagens.join('-_-'); // 'html-_-css-_-js-_-php-_-python'
+
+let htmlString = '<h2>Título Principal</h2>'
+htmlString = htmlString.split('h2');
+// ['<', '>Título Principal</', '>']
+htmlString = htmlString.join('h1');
+// <h1>Título Principal</h1>
+
+linguagens.slice(3); // ['php', 'python']
+linguagens.slice(1, 4); // ['css', 'js', 'php']
+
+const cloneLinguagens = linguagens.slice();
