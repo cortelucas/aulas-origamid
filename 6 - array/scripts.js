a1b62f24fec0849a0c00a4264d58c93dc541d5ae
Lucas Corte <corte1994.lc@gmail.com>
2021-05-23 14:05:13 -0300

    aula arrays 1

--- 6 - array/scripts.js
+++ 6 - array/scripts.js
@@ -1,29 +1,64 @@
-// Retorne um número aleatório
-// entre 1050 e 2000
-console.log(Math.floor(Math.random() * (2000 - 1050 + 1)) + 1050)
+const instrumentos = ['Guitarra', 'Baixo', 'Violão'];
+const precos = [49, 99, 69, 89];
 
-// Retorne o maior número da lista abaixo
-const numeros = '4, 5, 20, 8, 9';
-const arrayNumeros = numeros.split(', ');
-const numeroMax = Math.max(...arrayNumeros);
-console.log(numeroMax);
+const dados = [new String('Tipo 1'), ['Carro', 'Portas', {cor: 'Azul', preco: 2000}], function andar(nome) { console.log(nome) }];
 
+dados[2]('Ford');
+dados[1][2].cor; // azul
 
-// Crie uma função para limpar os preços
-// e retornar os números com centavos arredondados
-// depois retorne a soma total
-const listaPrecos = ['R$ 59,99', ' R$ 100,222',
-                     'R$ 230  ', 'r$  200'];
+const instrumentos = ['Guitarra', 'Baixo', 'Violão'];
+const carros = new Array('Corola', 'Mustang', 'Honda');
 
-function limparPreco(preco) {
-  preco = +preco.toUpperCase().replace('R$', '').trim().replace(',', '.');
-  preco = +preco.toFixed(2)
-  return preco;
+carros[1] // Mustang
+carros[2] = 'Ferrari';
+carros[10] = 'Parati';
+carros.length; // 11
+
+let li = document.querySelectorAll('li'); // NodeList
+Array.isArray(li); // false
+li = Array.from(li); // Array
+Array.isArray(li); // true
+
+const carros = {
+  0: 'Fiat',
+  1: 'Honda',
+  2: 'Ford',
+  length: 4,
 }
 
-let soma = 0;
-listaPrecos.forEach((i) => {
-  soma += limparPreco(i);
-});
+const carrosArray = Array.from(carros);
 
-console.log(soma.toLocaleString('pt-BR', {style: 'currency', currency:'BRL'}));
+Array.of(10); // [10]
+Array.of(1,2,3,4); // [1,2,3,4]
+new Array(5); // [,,,,]
+Array(5); // [,,,,]
+Array(1,2,3,4); // [1,2,3,4]
+
+
+const frutas = ['Banana', 'Pêra', ['Uva Roxa', 'Uva Verde']];
+frutas.length; // 3
+
+frutas[0].length; // 6
+frutas[1].length; // 5
+frutas[2].length; // 2
+
+instrumentos.sort(); //organiza por ordem alfabética
+instrumentos; // ['Baixo', 'Guitarra', Violão]
+
+const idades = [32,21,33,43,1,12,8]; //não organiza numeros
+idades.sort();
+idades; // [1, 12, 21, 32, 33, 43, 8]
+
+const carros = ['Ford', 'Fiat', 'VW'];
+carros.unshift('Honda', 'Kia'); // 5
+carros; // ['Honda', 'Kia', 'Ford', 'Fiat', 'VW'];
+
+carros.push('Ferrari'); // 6
+carros; // ['Honda', 'Kia', 'Ford', 'Fiat', 'VW', 'Ferrari'];
+const primeiroCarro = carros.shift(); // 'Ford'
+carros; // ['Fiat', 'VW', 'Honda'];
+
+const ultimoCarro = carros.pop(); // 'Honda'
+carros; // ['Fiat', 'VW'];
+
+carros.reverse(); // ['Honda', 'VW', 'Fiat', 'Ford'];
