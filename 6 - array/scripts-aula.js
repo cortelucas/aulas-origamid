71eb2e3a47c3670e555c3c6d3ad38b0dd3f9a5a3
Lucas Corte <corte1994.lc@gmail.com>
2021-05-23 14:07:36 -0300

    -r efatorando para exercicios

--- 6 - array/scripts-aula.js
+++ 6 - array/scripts-aula.js
@@ -1,82 +1,117 @@
-// ----- Number -----
-const preco = 59.49
-const gasolina = 5.499;
+const instrumentos = ['Guitarra', 'Baixo', 'Violão'];
+const precos = [49, 99, 69, 89];
 
-console.log(`
-  Number.isNaN - verifica se é number ou nao
-  Number.isNaN(NaN) = ${Number.isNaN(NaN)}
-  Number.isNaN(5 + 4) = ${Number.isNaN(5 + 4)}
+const dados = [new String('Tipo 1'), ['Carro', 'Portas', {cor: 'Azul', preco: 2000}], function andar(nome) { console.log(nome) }];
 
-  -----------------------------------------------
+dados[2]('Ford');
+dados[1][2].cor; // azul
 
-  Number.isInteger - verifica se é um number int
-  Number.isInteger(5) = ${Number.isInteger(5)}
-  Number.isInteger(5.4) = ${Number.isInteger(5.4)}
+const instrumentos = ['Guitarra', 'Baixo', 'Violão'];
+const carros = new Array('Corola', 'Mustang', 'Honda');
 
-  -----------------------------------------------
+carros[1] // Mustang
+carros[2] = 'Ferrari';
+carros[10] = 'Parati';
+carros.length; // 11
 
-  Number.parseFloat() = transforma String em number float
-  Number.parseFloat('324.452') = ${Number.parseFloat('324.452')}
-  Number.parseInt('23.49') = ${Number.parseInt('23.49')}
+let li = document.querySelectorAll('li'); // NodeList
+Array.isArray(li); // false
+li = Array.from(li); // Array
+Array.isArray(li); // true
 
-  -----------------------------------------------
+const carros = {
+  0: 'Fiat',
+  1: 'Honda',
+  2: 'Ford',
+  length: 4,
+}
 
-  toFixed() - retorna valor arredondado com base nas casa decimais
-  preco.toFixed() = ${preco.toFixed()}
-  gasolina.toFixed(2) = ${gasolina.toFixed(2)}
+const carrosArray = Array.from(carros);
 
-  -----------------------------------------------
+Array.of(10); // [10]
+Array.of(1,2,3,4); // [1,2,3,4]
+new Array(5); // [,,,,]
+Array(5); // [,,,,]
+Array(1,2,3,4); // [1,2,3,4]
 
-  toLocaleStrin() - formata numeros de acordo com a lingua passada
-  preco.toLocaleString('en-US', {style: 'currency', currency: 'USD'}) = ${preco.toLocaleString('en-US', {style: 'currency', currency: 'USD'})}
-  preco.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'}) = ${preco.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})}
-`);
 
-// ----- Math -----
-console.log(`
-  Math.PI - retorna o PI
-  Math.PI = ${Math.PI}
+const frutas = ['Banana', 'Pêra', ['Uva Roxa', 'Uva Verde']];
+frutas.length; // 3
 
-  -----------------------------------------------
-  
-  Math.abs() - Retorna valor absoluto;
-  Math.abs(-5.5) = ${Math.abs(-5.5)}
+frutas[0].length; // 6
+frutas[1].length; // 5
+frutas[2].length; // 2
 
-  ------------------------------------------------
+instrumentos.sort(); //organiza por ordem alfabética
+instrumentos; // ['Baixo', 'Guitarra', Violão]
 
-  Math.ceil() - Arredonda para inteiro para cima
-  Math.ceil(4.8334) =  ${Math.ceil(4.8334)}
-  Math.ceil(4.3) = ${Math.ceil(4.3)}
+const idades = [32,21,33,43,1,12,8]; //não organiza numeros
+idades.sort();
+idades; // [1, 12, 21, 32, 33, 43, 8]
 
-  ------------------------------------------------
+const carros = ['Ford', 'Fiat', 'VW'];
+carros.unshift('Honda', 'Kia'); // 5
+carros; // ['Honda', 'Kia', 'Ford', 'Fiat', 'VW'];
 
-  Math.floor - Aredonda para inteiro para baixo
-  Math.floor(4.8334) = ${Math.floor(4.8334)} 
-  Math.floor(4.3) = ${Math.floor(4.3)}
+carros.push('Ferrari'); // 6
+carros; // ['Honda', 'Kia', 'Ford', 'Fiat', 'VW', 'Ferrari'];
+const primeiroCarro = carros.shift(); // 'Ford'
+carros; // ['Fiat', 'VW', 'Honda'];
 
-  -------------------------------------------------
+const ultimoCarro = carros.pop(); // 'Honda'
+carros; // ['Fiat', 'VW'];
 
-  Math.round - Arredonda segundo regras matemáticas
-  Math.round(4.8334) = ${Math.round(4.8334)} 
-  Math.round(4.3) = ${Math.round(4.3)} 
+carros.reverse(); // ['Honda', 'VW', 'Fiat', 'Ford'];
 
-  -------------------------------------------------
+carros.splice(1, 0, 'Kia', 'Mustang'); // []
+carros; // ['Ford', 'Kia', 'Mustang', 'Fiat', 'VW', 'Honda']
 
-  Math.min e Math.max - retorna minimo e maximo
-  Math.max(5,3,10,42,2) = ${Math.max(5,3,10,42,2)}
-  Math.min(5,3,10,42,2) = ${Math.min(5,3,10,42,2)}
+carros.splice(3, 2, 'Ferrari'); // ['Fiat', 'VW']
+carros; // ['Ford', 'Kia', 'Mustang', 'Ferrari', 'Honda']
 
-  -------------------------------------------------
+['Item1', 'Item2', 'Item3', 'Item4'].copyWithin(2, 0, 3);
+// ['Item1', 'Item2', 'Item1', 'Item2']
 
-  Math.random() - retorna numero aleatorio de 0 a 1
-  Math.random() = ${Math.random()}
-  Math.floor(Math.random() * 100) = ${Math.floor(Math.random() * 100)}
-  Math.floor(Math.random() * 500) = ${Math.floor(Math.random() * 500)}
+['Item1', 'Item2', 'Item3', 'Item4'].copyWithin(-1);
+// ['Item1', 'Item2', 'Item3', 'Item1']
 
-  --------------------------------------------------
+['Item1', 'Item2', 'Item3', 'Item4'].fill('Banana');
+// ['Banana', 'Banana', 'Banana', 'Banana']
 
-  Número random entre 72 e 32
-  Math.floor(Math.random() * (72 - 32 + 1)) + 32 = ${Math.floor(Math.random() * (72 - 32 + 1)) + 32} 
-  Math.floor(Math.random() * (98 - 40 + 1)) + 40 = ${Math.floor(Math.random() * (98 - 40 + 1)) + 40}
+['Item1', 'Item2', 'Item3', 'Item4'].fill('Banana', 2);
+// ['Item1', 'Item2', 'Banana', 'Banana']
 
-`)
+['Item1', 'Item2', 'Item3', 'Item4'].fill('Banana', 1, 3);
+// ['Item1', 'Banana', 'Banana', 'Item4']
+
+const transporte1 = ['Barco', 'Aviao'];
+const transporte2 = ['Carro', 'Moto'];
+const transportes = transporte1.concat(transporte2);
+// ['Barco', 'Aviao', 'Carro', 'Moto'];
+
+const maisTransportes = [].concat(transporte1, transporte2, 'Van');
+// ['Barco', 'Aviao', 'Carro', 'Moto', 'Van'];
+
+const linguagens = ['html', 'css', 'js', 'php', 'python', 'js'];
+
+linguagens.includes('css'); // true
+linguagens.includes('ruby'); // false
+linguagens.indexOf('python'); // 4
+linguagens.indexOf('js'); // 2
+linguagens.lastIndexOf('js'); // 5
+
+const linguagens = ['html', 'css', 'js', 'php', 'python'];
+linguagens.join(); // 'html,css,js,php,python'
+linguagens.join(' '); // 'html css js php python'
+linguagens.join('-_-'); // 'html-_-css-_-js-_-php-_-python'
+
+let htmlString = '<h2>Título Principal</h2>'
+htmlString = htmlString.split('h2');
+// ['<', '>Título Principal</', '>']
+htmlString = htmlString.join('h1');
+// <h1>Título Principal</h1>
+
+linguagens.slice(3); // ['php', 'python']
+linguagens.slice(1, 4); // ['css', 'js', 'php']
+
+const cloneLinguagens = linguagens.slice();
