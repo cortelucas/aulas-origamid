d2eae25a7834f3c1b35777a6570aaf4217a91b7f
Lucas Corte <corte1994.lc@gmail.com>
2021-05-23 19:17:36 -0300

    aula 02 arrays e iteracoes

--- 7 - arrays e iteracao/scripts.js
+++ 7 - arrays e iteracao/scripts.js
@@ -60,3 +60,46 @@ const maiorNumero = numeros.reduce((acumulador, item) => {
   //   return item;
 },0);
 console.log(maiorNumero);
+
+const listaAulas = aulas.reduce((acumulador, atual, index) => {
+  acumulador[index] = atual.nome;
+  return acumulador
+}, {});
+console.log(listaAulas);
+
+/* passo a passo do reduce
+const aulas = [10, 25, 30];
+
+// 1
+aulas.reduce((0, 10) => {
+  return 0 + 10;
+}, 0); // retorna 10
+
+// 2
+aulas.reduce((10, 25) => {
+  return 10 + 25;
+}, 0); // retorna 35
+
+// 3
+aulas.reduce((35, 30) => {
+  return 35 + 30;
+}, 0); // retorna 65
+
+*/
+
+// ------ Array.some() ------
+const frutas = ['Banana', 'Pêra', 'Uva'];
+
+const temUva = frutas.some((item) => {
+  return item === 'Uva';
+});
+console.log(temUva);
+
+// ------ Array.every() ------
+const every = frutas.every((item) => {
+  return item === 'Uva';
+});
+console.log(every);
+
+const maiorQue3 = numeros.every(n  => n < 3);
+console.log(maiorQue3)
