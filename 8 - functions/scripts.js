d2fe9465f9ecc1cd2f64c9d5155037a1617a7891
Lucas Corte <corte1994.lc@gmail.com>
2021-05-24 10:26:14 -0300

    aula 2 de functions

--- 8 - functions/scripts.js
+++ 8 - functions/scripts.js
@@ -47,3 +47,35 @@ const arrayLike = {
 const filtro = Array.prototype.filter.call(li, (item) => {
   return item.classList.contains('ativo');
 })
+
+// ----- Function.prototype.aplly() -----
+const numeros = [33, 232, 33, 434, 54, 5424, 4];
+console.log(Math.max.apply(null, numeros));
+console.log(Math.max.call(null, 33, 232, 33, 434, 54, 5424, 4));
+
+// ----- Function.prototype.bind() -----
+//bind deve-se ativar a função
+const filtro2 = Array.prototype.filter.bind(li, (item) => {
+  return item.classList.contains('ativo');
+})
+console.log(filtro2());
+
+const $ = document.querySelectorAll.bind(document);
+
+const carro = {
+  marca: 'Ford',
+  ano: 2018,
+  acelerar: function(aceleracao, tempo) {
+    return `${this.marca} acelerou ${aceleracao} em ${tempo}`;
+  }
+}
+carro.acelerar(100, 20);
+
+function imc(altura, peso) {
+  return peso / (altura * altura);
+}
+
+const imc180 = imc.bind(null, 1.80);
+
+imc(1.80, 70); // 21.6
+imc180(70); // 21.6
