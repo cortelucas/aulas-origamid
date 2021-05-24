679882a43264e9f480e065f08819dae02a0ec248
Lucas Corte <corte1994.lc@gmail.com>
2021-05-24 12:32:04 -0300

    exe 01

--- 8 - functions/scripts.js
+++ 8 - functions/scripts.js
@@ -1,6 +1,11 @@
 // Retorne a soma total de caracteres dos
 // parágrafos acima utilizando reduce
+const ps = document.querySelectorAll('p');
+const totalCaracteres = Array.prototype.reduce.call(ps, (cont, p) => {
+  return cont + p.innerText.length;
+}, 0);
 
+console.log(totalCaracteres);
 
 // Crie uma função que retorne novos elementos
 // html, com os seguintes parâmetros
