37889e370c62e7ee59155af1a1187bd2da6eba5b
Lucas Corte <corte1994.lc@gmail.com>
2021-05-25 12:50:09 -0300

    exe-01

--- 9 - object/scripts.js
+++ 9 - object/scripts.js
@@ -1,5 +1,9 @@
 // Crie uma função que verifique
 // corretamente o tipo de dado
+function verficarDado(dado) {
+  return Object.prototype,toString.call(dado);
+}
+console.log(verficarDado({})); //[object Object]
 
 // Crie um objeto quadrado com
 // a propriedade lados e torne
